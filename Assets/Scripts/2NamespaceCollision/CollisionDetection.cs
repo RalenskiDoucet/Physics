cd88755a7794f28b29053de3aa2546e909d3fae8
Ralenski Doucet <Ralenskid@gmail.com>
2018-11-13 17:50:46 -0600

    Started Make Collision Detection

---
+++ Assets/Scripts/2NamespaceCollision/CollisionDetection.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Collision
+{
+    public class CollisionDetection : CollisionVolume
+    { 
+        CollisionVolume mShape = new CollisionVolume();
+        CollisionVolume mShape2 = new CollisionVolume();
+        BoxCollider2D box1;
+        
+        public bool collision_made;
+
+        void check_for_collision(List<float> x ,List<float> y)
+        {
+            CollisionVolume mShape = new CollisionVolume();
+            CollisionVolume mShape2 = new CollisionVolume();
+
+            if (mShape2.x_min<mShape.x_max)
+            {
+                collision_made = true;
+                BroadcastMessage("Collision was made");
+            }
+            if (mShape2.y_min<mShape.y_max)
+            {
+                collision_made = true;
+            }
+        }
+        // Use this for initialization
+        void Start() {
+            check_for_collision(x_Values, y_Values);
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
---
+++ Assets/Scripts/2NamespaceCollision/CollisionVolume.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Collision
+{
+    public class CollisionVolume : MonoBehaviour
+    {
+       
+        public float x_min;
+        public float x_max;
+        public float y_min;
+        public float y_max;
+        public List<float> x_Values;        
+        public List<float> y_Values;
+        void add_Values_to_list()
+        {
+            x_Values.Add(x_max);
+            x_Values.Add(x_min);
+            y_Values.Add(y_min);
+            y_Values.Add(y_max);
+        }
+        // Use this for initialization
+        void Start()
+        {
+            add_Values_to_list();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
